46943e72229db59fca7a7ab5c68e1c76f1885431
akshaydhayal <akshaydhayalkd99@gmail.com>
2026-01-13 17:01:43 +0530

    active stake accounts fetch done

--- frontend/src/components/AdminStakeAccounts.tsx
+++ frontend/src/components/AdminStakeAccounts.tsx
@@ -2,51 +2,60 @@ import { useConnection } from '@solana/wallet-adapter-react';
 import { TrendingUp } from 'lucide-react'
 import * as borsh from "borsh";
 import { lstManagerPda, PROGRAM_ID, stakeRegistryRecordPda } from '../lib/constants';
-import { StakeRegistryRecordSchema, valueToU64Schema } from '../lib/borshSchema';
+import { stakeAccountsSchema, StakeRegistryRecordSchema, valueToU64Schema } from '../lib/borshSchema';
 import { Buffer } from 'buffer';
-import { PublicKey } from '@solana/web3.js';
+import { LAMPORTS_PER_SOL, PublicKey, StakeProgram } from '@solana/web3.js';
+import { useEffect, useState } from 'react';
+
+type stakeAccountsType={index:number,pubkey:string, stakeAmount:number, activatedEpoch:number};
 
 const AdminStakeAccounts = () => {
   let {connection}=useConnection();
-  const stakeAccounts0 = [
-    { index: 0, address: '7xJ9...kL2p', amount: 10000, status: 'active', epoch: 1005 },
-    { index: 1, address: '9mK3...nP4r', amount: 15000, status: 'active', epoch: 1005 },
-    { index: 2, address: '4tH8...qR7s', amount: 20000, status: 'active', epoch: 1004 },
-  ];
-  async function getAllActiveStakeAccounts(){
-    let stakeRegistryPdaData=await connection.getAccountInfo(stakeRegistryRecordPda);
-    let deserialisedStakeRegistryPdaData=borsh.deserialize(StakeRegistryRecordSchema, stakeRegistryPdaData?.data);
-    let totalStakeAccCount=Number(deserialisedStakeRegistryPdaData.next_stake_index)-1; 
-
-    let stakeAccounts:PublicKey[]=[];
-    for(let i=1; i<=totalStakeAccCount; i++){
-        let serialisedStakeIndex=borsh.serialize(valueToU64Schema, {value:i});    
-        let [nextStakeAccPda,nextStakeAccBump]=PublicKey.findProgramAddressSync([Buffer.from("stake_acc"), Buffer.from(serialisedStakeIndex), lstManagerPda.toBuffer()], PROGRAM_ID)
-        stakeAccounts.push(nextStakeAccPda.toBase58());
+  const [activeStakeAccounts,setActiveStateAccounts]=useState<stakeAccountsType[]>([]);
+  
+  useEffect(()=>{
+    async function getAllActiveStakeAccounts(){
+      let stakeRegistryPdaData=await connection.getAccountInfo(stakeRegistryRecordPda);
+      let deserialisedStakeRegistryPdaData=borsh.deserialize(StakeRegistryRecordSchema, stakeRegistryPdaData?.data);
+      let totalStakeAccCount=Number(deserialisedStakeRegistryPdaData.next_stake_index)-1; 
+  
+      let stakeAccount:stakeAccountsType[]=[];
+      for(let i=1; i<=totalStakeAccCount; i++){
+          let serialisedStakeIndex=borsh.serialize(valueToU64Schema, {value:i});    
+          let [stakeAccPda,stakeAccBump]=PublicKey.findProgramAddressSync([Buffer.from("stake_acc"), Buffer.from(serialisedStakeIndex), lstManagerPda.toBuffer()], PROGRAM_ID)
+          
+          let stakeAccPdaData=await connection.getAccountInfo(stakeAccPda);
+          let deserialisedStakeAccData=borsh.deserialize(stakeAccountsSchema, stakeAccPdaData?.data);
+          if(deserialisedStakeAccData.stake_discriminator==2){
+            stakeAccount.push({
+              index:i,
+              pubkey:stakeAccPda.toBase58(),
+              stakeAmount:Number(deserialisedStakeAccData.stake_delegation_stake_amount),
+              activatedEpoch:Number(deserialisedStakeAccData.stake_delegation_activation_epoch)
+            });
+          }
+        }
+      setActiveStateAccounts(stakeAccount);
+      console.log("stakeAccount : ",stakeAccount);
     }
-    console.log("stakeAccounts : ",stakeAccounts);
-  }
-  getAllActiveStakeAccounts();
+    getAllActiveStakeAccounts();
+  },[connection])
 
   return (
     <div className="bg-gray-800/50 backdrop-blur-sm rounded-2xl border border-gray-700/50 p-6">
         <h3 className="text-lg font-semibold mb-4 flex items-center gap-2">
-        <TrendingUp size={20} className="text-green-400" />
-        Active Stake Accounts
-        </h3>
+          <TrendingUp size={20} className="text-green-400" />Active Stake Accounts</h3>
         <div className="space-y-3">
-        {stakeAccounts0.map((acc) => (
-            <div key={acc.index} className="bg-gray-900/50 rounded-lg p-3 border border-gray-700/50">
-            <div className="flex justify-between items-start mb-2">
-                <div className="font-mono text-xs text-gray-400">{acc.address}</div>
-                <span className="px-2 py-0.5 bg-green-600/20 text-green-400 text-xs rounded-full border border-green-600/30">
-                Active
-                </span>
-            </div>
-            <div className="text-lg font-semibold">{acc.amount.toLocaleString()} SOL</div>
-            <div className="text-xs text-gray-400 mt-1">Epoch: {acc.epoch}</div>
-            </div>
-        ))}
+          {activeStakeAccounts.map((acc) => (
+              <div key={acc.index} className="bg-gray-900/50 rounded-lg p-3 border border-gray-700/50">
+                <div className="flex justify-between items-start mb-2">
+                    <div className="font-mono text-xs text-gray-400 truncate">{acc.pubkey}</div>
+                    <span className="px-2 py-0.5 bg-green-600/20 text-green-400 text-xs rounded-full border border-green-600/30">Active</span>
+                </div>
+                <div className="text-lg font-semibold">{(acc.stakeAmount/LAMPORTS_PER_SOL)} SOL</div>
+                <div className="text-xs text-gray-400 mt-1">Epoch: {acc.activatedEpoch}</div>
+              </div>
+          ))}
         </div>
     </div>
   )
--- frontend/src/lib/borshSchema.ts
+++ frontend/src/lib/borshSchema.ts
@@ -28,3 +28,26 @@ export let StakeRegistryRecordSchema:borsh.Schema={
 export let valueToU64Schema:borsh.Schema={
     struct:{value:'u64'}
 }
+
+//valid only for stake discrimintaor 2, staked type
+export let stakeAccountsSchema:borsh.Schema={
+    struct:{
+        stake_discriminator:'u32',
+
+        meta_rent_exempt_resever:'u64',
+        meta_auth_staker:{array:{type:'u8',len:32}},
+        meta_auth_withdrawer:{array:{type:'u8',len:32}},
+        meta_lockup_unix_timestamp:'u64',
+        meta_lockup_epoch:'u64',
+        meta_lockup_custodian:{array:{type:'u8',len:32}},
+
+        stake_delegation_voter_acc:{array:{type:'u8',len:32}},
+        stake_delegation_stake_amount:'u64',
+        stake_delegation_activation_epoch:'u64',
+        stake_delegation_deactivation_epoch:'u64',
+        stake_delegation_warmup_cooldown_rate:'u64',
+        stake_credits_observed:'u64',
+
+        stake_flags:'u32'
+    }
+}
